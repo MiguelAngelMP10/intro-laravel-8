7509c5f44f481f85a41f4a38ee13c3896cef05b2
MiguelAngelMP10 <mmunozpozos@gmail.com>
2021-02-01 14:48:00 -0600

    Index y Layout: definiendo estructura HTML

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -1,1 +1,16 @@
-ss
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cursos de Programación Web</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}"
+</head>
+<body>
+  <main class="py-10">
+    <div class="container mx-auto px-4">
+      @yield('content')
+    </div>
+  </main>
+</body>
+</html>
