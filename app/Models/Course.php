f0fb7e2226462df2276ec01e156830dac02b9111
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-05-23 01:54:08 -0500

    Livewire: diseño del componente

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -13,4 +13,9 @@ class Course extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getExcerptAttribute()
+    {
+        return substr($this->description, 0, 80) . "...";
+    }
 }
